3f44a65c33185dade7f3d9b36bdd38f454e4a087
beagle27866 <bamaral2.git2023@gmail.com>
2024-09-19 15:44:57 -0400

    Update: component footer are done

--- Book_Palette/src/App.jsx
+++ Book_Palette/src/App.jsx
@@ -1,13 +1,15 @@
 import { Header } from "./Components/Header";
+import { Footer } from "./Components/Footer";
+import { Navbar } from "./Components/Navbar";
 import styles from './App.module.css';
 import './global.css';
-import { Footer } from "./Components/Footer";
 
 export function App() {
 
   return (
     <div className={styles.container}>
       <Header />
+      <Navbar />
       <Footer />
       <div className={styles.copyright}>
         Copyright &copy; 2024 BeagleProg. All Rights Reserved. Made by Bruno Amaral
